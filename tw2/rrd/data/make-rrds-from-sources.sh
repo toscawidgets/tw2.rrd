92f300b8a76cafcc5e302d34039baa7c98992252
Ralph Bean <ralph.bean@gmail.com>
2011-05-31 11:27:13 -0400

    Script to convert from __xml__ to .rrd

---
+++ tw2/rrd/data/make-rrds-from-sources.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script to convert xml sources in __xml__ to the local arch.
+
+arch=$(uname -i);
+if [ "$arch" == "x86_64" ] ; then
+        ARCH=64;
+elif [ "$arch" == "x86_32" ] ; then
+        ARCH=32;
+else
+        echo "Unrecognized arch $arch";
+        exit 1;
+fi
+
+echo "Detected arch $ARCH."
+
+for pth in $(find __xml__ -type f) ; do
+        target=$(echo $pth | sed "s/__xml__/$ARCH/" | sed "s/.rrd.xml/.rrd/")
+        echo $pth to $target;
+        rm -f $target;
+        rrdtool restore $pth $target
+done
