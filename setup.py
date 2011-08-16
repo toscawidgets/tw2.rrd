afd5f7a3eab0d8ac75f49d7da2c9baa7a0c462dc
Ralph Bean <ralph.bean@gmail.com>
2011-08-16 11:41:47 -0400

    Version bump.  Thanks tmu.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ f.close()
 
 setup(
     name='tw2.rrd',
-    version='2.0b13',
+    version='2.0b14',
     description='tw2/rrdtool mashups!  plot your rrdtool data on the web',
     long_description=long_description,
     author='Ralph Bean',
